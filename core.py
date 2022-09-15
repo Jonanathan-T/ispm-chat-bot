826b0e4104f2ad20029b3fa25d3bcbe690103d72
Totozamany Jonathan <53228019+Jonanathan-T@users.noreply.github.com>
2022-09-15 12:04:09 +0300

    Ajout des details pour chaque parcours

--- core.py
+++ core.py
@@ -43,49 +43,96 @@ mentions = [
     {
         "mention":"Informatique et Telecommunications", 
         "parcours":[
-            "Informatique de Gestion, Genie Logiciel et Intelligence Artificielle",
-            "Electronique Systemes Informatiques et Intelligence Artificielle",
-            "Informatique, Multimedia, Technologie de l'Information, de la Communication et Intelligence Artificielle",
-            "Informatique, Statistiques Appliquees et Intelligence Artificielle",
-        ]
+           {
+               "leash": "Informatique de Gestion, Genie Logiciel et Intelligence Artificielle",
+               "detail": "Toutes les entreprises (publiques ou privées) ne peuvent plus se passer de l'outil informatique surtout l'informatique appliquée à la gestion. La filière Informatique de Gestion Génie Logiciel et Intelligence Artificielle est une filière dont l'objectif est la formation d'Ingénieurs et de Techniciens Supérieurs capables de maîtriser toutes les techniques informatiques relatives à la gestion des entreprises.",
+           },
+           {
+               "leash": "Electronique Systemes Informatiques et Intelligence Artificielle",
+                "detail": "L'informatique n'est pas seulement le software. L'électronique est avant tout l'origine de l'informatique. Elle est aussi à l'origine de la technologie des télécommunications. Ainsi, c'est dans cette filière que l'ISPM donne une formation d'Ingénieurs et de Techniciens Supérieurs qui seront capables de maîtriser à la fois l'électronique, la structure des ordinateurs et des applications fondamentales de l'informatique.",
+           },
+           {
+               "leash": "Informatique, Multimedia, Technologie de l'Information, de la Communication et Intelligence Artificielle",
+                "detail": "C'est une filière dont la finalité est la formation des étudiants dans l'informatique multimédia, la télécommunication et d'une manière générale dans les nouvelles technologies de l'information et de la télécommunication.",
+           },
+           {
+               "leash": "Informatique, Statistiques Appliquees et Intelligence Artificielle",
+                "detail": "C'est une filière dont le but est de focaliser sur l'application des méthodes statistiques avec les méthodes informatiques dans les divers domaines de l'Economie. Le cursus est basé sur un solide apprentissage des Mathématiques avec les éléments Informatiques nécessaires. A l'issue de cette formation, les étudiants peuvent travailler dans diverses branches de l'économie : les banques, les entreprises industrielles, les entreprises commerciales.",
+           },
+        ],
+
     },
     {
         "mention":"Droit et Techniques des Affaires",
         "parcours":[
-            "Commerce et Administration des Affaires",
-            "Droit et Techniques Juridiques des Affaires",
-            "Finances et Comptabilites",
-            "Economie et Management de Projet"
+           {
+               "leash": "Commerce et Administration des Affaires",
+               "detail": "Madagascar comme presque tous les pays du monde a opté pour l'économie libérale où la concurrence surtout sur le plan économique et commercial est très important. A cela s'ajoute le contexte de mondialisation et de globalisation actuelles. C'est dans ce sens que nous avons créé la filière commerce et administration des affaires qui est une filière de formation fondée sur le marketing et les différentes techniques commerciales ainsi que les différentes techniques d'organisation et de management des entreprises.",
+           },
+           {
+               "leash": "Droit et Techniques Juridiques des Affaires",
+               "detail": "Dans cette filière, l'ISPM veut former des juristes capables de maîtriser toutes les techniques juridiques à la fois nationales et internationales. L'originalité de cette filière est qu'elle utilise l'outil informatique. Les techniciens du droit formés par l'ISPM pourront travailler soit dans l'administration publique soit dans les entreprises privées.",
+           },
+           {
+               "leash": "Finances et Comptabilites",
+               "detail": "Toute entreprise qu'elle soit publique ou privée ne peut se passer des services offerts par les départements finances et comptabilités. Ce sont les départements clés d'une entreprise. C'est dans ce sens que nous avons créé cette filière dont l'objectif est de former des étudiants dans les différentes techniques quantitatives de la gestion des entreprises telles que les finances et la comptabilité.",
+           },
+           {
+               "leash": "Economie et Management de Projet",
+               "detail": "La maîtrise des techniques économiques est indispensable aux dirigeants de l'administration publique et des entreprises privées à tous les niveaux. Dans cette filière, l'ISPM veut former des économistes capables de faire des analyses économiques objectives, relationnelles et de les appliquer aux réalités des entreprises.",
+           },
         ],
     },
     {
         "mention":"Biotechnologie et Agronomie",
         "parcours":[
-            "Industries Agro-Alimentaires",
-            "Pharmacologie et Industrie Pharmaceutiques",
-            "Agriculture et Elevage"
+           {
+               "leash": "Industries Agro-Alimentaires",
+               "detail": "Les différentes industries agroalimentaires ont une place et un rôle important dans notre économie. L'ISPM a comme objectif de former des cadres de qualité pour ces entreprises.",
+           },
+           {
+               "leash": "Pharmacologie et Industrie Pharmaceutiques",
+               "detail": "L'existence des plantes médicinales endémiques est une réalité à Madagascar. L'ISPM veut mettre en valeur les propriétés therapeutiques de ces plantes d'une manière scientifique.",
+           },
+           {
+               "leash": "Agriculture et Elevage",
+               "detail": "Plus de 80% de Malagasy vivent et travaillent dans le monde rural. Par ailleurs, il est clair que le secteur primaire est un des secteurs fondamentaux dans l'économie d'un pays. C'est dans ce sens qu'a été créée la filière Agriculture et élevage car il nous faut des jeunes capables d'appliquer les techniques et les technologies modernes dans le monde rural. Ils sont aussi formés au concept de l'agri-business.",
+           },
         ],
     },
     {
         "mention":"Genie Industriel",
         "parcours":[
-            "Electromecanique et Informatique Industriel",
-            "Industries Chimiques",
-            "Industries Minieres et Petrolieres"
+           {
+               "leash": "Electro-Mecanique et Informatique Industriel",
+               "detail": "Toute grande école polytechnique qui se respecte doit avoir un département d'éléctro-mécanique. D'ailleurs, la révolution industrielle n'a été possible qu'à travers la maîtrise de la mécanique et des technologies industrielles. ISPM par le biais de son département Electro-Mécanique et Informatique Industrielle (EMII) forme des jeunes capables de maîtriser toutes les technologies mentionnées ci-dessus auxquelles s'ajoutent l'informatique et l'informatique industrielle. A l'issu de leurs études, ils seront capables de participer au développement industriel de Madagascar."
+           },
+           {
+               "leash": "Industries Chimiques, Minieres et Petrolieres",
+               "detail": "Dans le contexte actuel, tout pays qui se respecte doit avoir des jeunes capables de maîtriser les différents domaines tels que les Industries Chimiques, Minières et Pétrolières. C'est dans ce sens nous avons créé cette filière.",
+           },
         ],
     },
     {
         "mention":"Genie Civil et Architecture",
         "parcours":[
-            "Genie Civil",
-            "Architecture"
+            {
+                "leash": "Genie Civil et Architecture",
+                "detail": "La construction des infrastructures est un élément fondamental pour améliorer le niveau de vie d'un pays. C'est pourquoi l'ISPM forme à travers sa filière Génie Civil et Architecture les jeunes qui vont améliorer non seulement les infrastructures mais participer à l'aménagement urbain et rural."
+            },
         ],
     },
     {
         "mention":"Tourisme",
         "parcours":[
-            "Tourisme et Environnement",
-            "Tourisme et Hotellerie"
+            {
+                "leash":"Tourisme et Environnement",
+                "detail": "Madagascar est un des rares pays au monde à avoir un environnement unique et endémique par sa faune et sa flore. Par ailleurs, la civilisation malagasy est aussi unique par l'apport des cultures à la fois asiatiques, africaines et plus tard européennes. La filière tourisme et environnement a pour objectifs d'enseigner aux étudiants à la fois la richesse de notre environnement et de notre civilisation. Car ces étudiants seront les futurs professionnels du secteur \"tourisme\".",
+            },
+            {
+                "leash":"Tourisme et Hotellerie",
+                "detail": "La civilisation malagasy est unique par l'apport des cultures à la fois asiatiques, africaines et plus tard européennes. La filière tourisme et hôtellerie a pour objectifs d'enseigner aux étudiants la richesse de notre civilisation. Par ailleurs, les autres cultures ne seront pas négligées car l'ISPM forme des étudiants capables de maîtriser les différentes techniques de l'art culinaire à la fois national et international.",
+            },
         ],
     }
 ]
@@ -151,4 +198,13 @@ def career(sender_id, lang, cmd, parcours, **extends):
     i = 0
     for parcour in parcours:
         i = i + 1
-        chat.send_message(sender_id, "{} - {}".format(i,parcour))
+        chat.send_message(sender_id, f"{i} - {parcour['leash']}")
+    query.set_action(sender_id, Payload('/details', parcours=parcours))
+
+@ampalibe.action('/details')
+def details(sender_id, lang, parcours, cmd, **extends):
+    query.set_action(sender_id, None)
+    
+    '''
+        Print detail of parcour checked
+    '''
